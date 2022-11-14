467d1fc541147d5ea626898e36a8aeabc7bffedd
Marc Aldrich <marcaldrich@gmail.com>
2022-11-14 06:38:50 -0600

    Implement dinosaur. No tests currently needed as it is only a data class. Filter tests and queries are handled at the park abstraction.

--- dinosaur.go
+++ dinosaur.go
@@ -1,7 +1,57 @@
 package dinopark
 
-type Dinosaur struct{}
+type Dinosaur struct {
+	Name    string
+	Species DinoSpecies
+}
 
-type DinoFilter struct{}
+type DinoFilter struct {
+	ByDiet    string
+	BySpecies string
+	ByName    string
+}
 
-//TODO: implement kind
+type DinoSpecies uint16
+
+const (
+	Tyrannosaurus = iota
+	Velociraptor
+	Spinosaurus
+	Megalosaurus
+	Brachiosaurus
+	Stegosaurus
+	Ankylosaurus
+	Triceratops
+)
+
+type DinoDiet uint16
+
+const (
+	Omnivore = iota
+	Carnivore
+	Herbivore
+)
+
+func (d *Dinosaur) DinoDietType() DinoDiet {
+	var dDiet DinoDiet
+	switch d.Species {
+	case Tyrannosaurus:
+		dDiet = Carnivore
+	case Velociraptor:
+		dDiet = Carnivore
+	case Spinosaurus:
+		dDiet = Carnivore
+	case Megalosaurus:
+		dDiet = Carnivore
+	case Brachiosaurus:
+		dDiet = Herbivore
+	case Stegosaurus:
+		dDiet = Herbivore
+	case Ankylosaurus:
+		dDiet = Herbivore
+	case Triceratops:
+		dDiet = Herbivore
+	}
+
+	return dDiet
+}
